a8ac780d4e47f677fc1835353e58d6bca9008fc1
Al Zee <z@alz.ee>
2020-12-28 17:45:51 +0800

    home task section use data from ng

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,4 +12,24 @@ export class HomePage implements OnInit {
   ngOnInit() {
   }
 
+  public tasks = [
+    {
+      boss: 'jess',
+      title: '任务名称',
+      runner: 88,
+      left: 12,
+      cate: ['reg'],
+      sticky: true,
+      price: 55.5,
+    },
+    {
+      boss: 'mike',
+      title: '任务名称',
+      runner: 88,
+      left: 12,
+      cate: ['reg', 'save'],
+      sticky: false,
+      price: 55.5,
+    },
+  ];
 }
