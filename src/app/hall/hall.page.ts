ed74af28bcf6d9fee252ed4dcf0024a09fb23fe2
Al Zee <z@alz.ee>
2020-12-29 14:53:06 +0800

    page hall done

--- src/app/hall/hall.page.ts
+++ src/app/hall/hall.page.ts
@@ -12,4 +12,56 @@ export class HallPage implements OnInit {
   ngOnInit() {
   }
 
+  public sorts= [
+    {
+      value: 'all',
+      label: '悬赏大厅',
+    },
+    {
+      value: 'date',
+      label: '最新任务',
+    },
+    {
+      value: 'price',
+      label: '高价任务',
+    },
+  ];
+  public cate = [
+    '下载APP',
+    '账号注册',
+    '认证绑卡',
+    '电商相关',
+    '简单关注',
+    '简单转发',
+    '投票/砍价',
+    '推广引流',
+    '其它任务',
+  ];
+  public tasks = [
+    {
+      boss: 'jess',
+      avatar: '../assets/img/she.png',
+      title: '任务名称',
+      runner: 88,
+      left: 12,
+      cate: ['reg'],
+      sticky: true,
+      price: 55.5,
+    },
+    {
+      boss: 'mike',
+      avatar: '../assets/img/she.png',
+      title: '任务名称',
+      runner: 88,
+      left: 12,
+      cate: ['reg', 'save'],
+      sticky: false,
+      price: 55.5,
+    },
+  ];
+
+  customActionSheetOptions: any = {
+    header: 'Colors',
+    subHeader: 'Select your favorite color'
+  };
 }
