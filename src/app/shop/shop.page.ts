4e4d757a744abad6e0e37e3dd8a3cd65589d4bf0
Al Zee <z@alz.ee>
2021-01-11 00:20:54 +0800

    page shop

--- src/app/shop/shop.page.ts
+++ src/app/shop/shop.page.ts
@@ -12,4 +12,51 @@ export class ShopPage implements OnInit {
   ngOnInit() {
   }
 
+  public stats = [
+    {
+      label: "保证金",
+      value: 250,
+    },
+    {
+      label: "发布任务",
+      value: 50,
+    },
+    {
+      label: "接任务",
+      value: 150,
+    },
+    {
+      label: "被投诉",
+      value: 2,
+    },
+    {
+      label: "申请",
+      value: 20,
+    },
+  ];
+
+  public tasks = [
+    {
+      boss: 'jess',
+      avatar: '../assets/img/she.png',
+      title: '任务名称',
+      runner: 88,
+      left: 12,
+      cate: ['reg'],
+      sticky: true,
+      price: 55.5,
+      link: '/detail',
+    },
+    {
+      boss: 'mike',
+      avatar: '../assets/img/she.png',
+      title: '任务名称',
+      runner: 88,
+      left: 12,
+      cate: ['reg', 'save'],
+      sticky: false,
+      price: 55.5,
+      link: '/detail',
+    },
+  ];
 }
