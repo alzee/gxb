46d94808444f965aeb181c330880d6391b0480d4
Al Zee <z@alz.ee>
2021-01-11 01:05:19 +0800

    picker in page land

--- src/app/land/land.page.ts
+++ src/app/land/land.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from "@ionic/angular";
+import { PickerOptions } from "@ionic/core";
 
 @Component({
   selector: 'app-land',
@@ -7,11 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandPage implements OnInit {
 
-  constructor() { }
+  constructor(private pickerController: PickerController) {}
 
   ngOnInit() {
   }
 
-  public cellNum = 35;
+  const multiColumnOptions = [
+    [
+      '北京',
+      '上海',
+      '湖北',
+      '湖南',
+    ],
+    [
+      '武汉',
+      '十堰',
+      '宜昌',
+      '襄阳',
+    ],
+    [
+      '张湾区',
+    ]
+  ]
+
+  async openPicker(numColumns = 1, numOptions = 5, columnOptions = multiColumnOptions){
+    const picker = await this.pickerController.create({
+      columns: this.getColumns(numColumns, numOptions, columnOptions),
+      buttons: [
+        {
+          text: '取消',
+          role: 'cancel'
+        },
+        {
+          text: '确定',
+          handler: (value) => {
+            console.log(`Got Value ${value}`);
+          }
+        }
+      ]
+    });
+
+    await picker.present();
+  }
+
+  getColumns(numColumns, numOptions, columnOptions) {
+    let columns = [];
+    for (let i = 0; i < numColumns; i++) {
+      columns.push({
+        name: `col-${i}`,
+        options: this.getColumnOptions(i, numOptions, columnOptions)
+      });
+    }
+
+    return columns;
+  }
+
+  getColumnOptions(columnIndex, numOptions, columnOptions) {
+    let options = [];
+    for (let i = 0; i < numOptions; i++) {
+      options.push({
+        text: columnOptions[columnIndex][i % numOptions],
+        value: i
+      })
+    }
+
+    return options;
+  }
 
 }
