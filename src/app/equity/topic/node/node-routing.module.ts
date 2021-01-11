d6d852fd70a384b37e718e48ea6545d0f2d94dd3
Al Zee <z@alz.ee>
2021-01-11 09:02:00 +0800

    page equity/topic/node

--- src/app/equity/topic/node/node-routing.module.ts
+++ src/app/equity/topic/node/node-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TopicPage } from './topic.page';
+import { NodePage } from './node.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: TopicPage
-  },
-  {
-    path: 'node',
-    loadChildren: () => import('./node/node.module').then( m => m.NodePageModule)
+    component: NodePage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class TopicPageRoutingModule {}
+export class NodePageRoutingModule {}
