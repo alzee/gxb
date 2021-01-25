ac6e173b5290a2be69d835ddc9173492ddbb03c4
Al Zee <z@alz.ee>
2021-01-25 12:29:27 +0800

    page signin actions

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthConstants } from '../config/auth-constants';
+import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
+import { ToastService } from '../services/toast.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
+    postData = {
+        username: '',
+        password: ''
+    };
 
-  constructor() { }
+    constructor(
+        private router: Router,
+        private authService: AuthService,
+        private storageService: StorageService,
+        private toastService: ToastService
+    ) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
+
+    validateInputs() {
+        let username = this.postData.username.trim();
+        let password = this.postData.password.trim();
+        return (
+            this.postData.username &&
+                this.postData.password &&
+                username.length > 0 &&
+                password.length > 0
+        );
+    }
+
+    loginAction() {
+        if (this.validateInputs()) {
+            this.authService.login(this.postData).subscribe(
+                (res: any) => {
+                    if (res.userData) {
+                        // Storing the User data.
+                        this.storageService.store(AuthConstants.AUTH, res.userData);
+                        this.router.navigate(['home']);
+                    } else {
+                        this.toastService.presentToast('用户名或密码错误');
+                    }
+                },
+                (error: any) => {
+                    this.toastService.presentToast('网络异常');
+                }
+            );
+        } else {
+            this.toastService.presentToast('请输入用户名和密码');
+        }
+    }
 
 }
