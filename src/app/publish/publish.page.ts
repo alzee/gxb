e8a25261f8198b8cdd964151c3e583a433af0aa6
Al Zee <z@alz.ee>
2021-02-01 19:31:32 +0800

    now we can post successfully

--- src/app/publish/publish.page.ts
+++ src/app/publish/publish.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { environment } from '../../environments/environment';
+import { AuthConstants } from '../config/auth-constants';
+import { StorageService } from '../services/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-publish',
@@ -6,53 +11,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publish.page.scss'],
 })
 export class PublishPage implements OnInit {
+  userData = {};
+  category = '';
+  platform = '';
+  categories = [];
+  platforms = [];
+  postData = {
+      owner: '',
+      title: '',
+      name: '',
+      applydays: '',
+      approvedays: '',
+      prepaid: '',
+      quantity: '',
+      showdays: '',
+      category: '',
+      platform: ''
+  };
 
-  constructor() { }
+  constructor(
+      private httpService: HttpService,
+      private storageService: StorageService,
+      private router: Router
+  ) {
+      this.storageService.get(AuthConstants.AUTH).then((res) => {
+          this.userData = res;
+      });
+
+      this.httpService.get('categories?itemsPerPage=50', this.postData).subscribe((res) => {
+          console.log(res);
+          this.categories = res;
+      });
+      this.httpService.get('platforms?itemsPerPage=50', this.postData).subscribe((res) => {
+          console.log(res);
+          this.platforms = res;
+      });
+  }
 
   ngOnInit() {
   }
 
-  log(o) {
-      console.log(o.el.className.indexOf('radio-checked'));
+  postTask() {
+      this.validateInputs();
+      this.httpService.post('tasks', this.postData).subscribe((res) => {
+          console.log(res);
+          this.router.navigate(['/myposts']);
+      });
   }
-  public cate = [
-    {
-      label: '下载APP',
-      cate: 'app',
-    },
-    {
-      label: '账号注册',
-      cate: 'reg',
-    },
-    {
-      label: '认证绑卡',
-      cate: 'authen',
-    },
-    {
-      label: '电商相关',
-      cate: 'ecomm',
-    },
-    {
-      label: '简单关注',
-      cate: 'follow',
-    },
-    {
-      label: '简单转发',
-      cate: 'forward',
-    },
-    {
-      label: '投票/砍价',
-      cate: 'poll',
-    },
-    {
-      label: '推广引流',
-      cate: 'promo',
-    },
-    {
-      label: '其它任务',
-      cate: 'other',
-    },
-  ];
 
-  public devices = ['全部', '安卓', '苹果'];
+  validateInputs() {
+      this.postData.owner = 'api/users/' + this.userData.id;
+      this.postData.applydays = parseInt(this.postData.applydays, 10);
+      this.postData.category = '/api/categories/' + this.category;
+      this.postData.platform = '/api/platforms/' + this.platform;
+      let title = this.postData.title.trim();
+      let name = this.postData.name.trim();
+      let applydays = this.postData.applydays;
+      let approvedays = this.postData.approvedays;
+      let prepaid = this.postData.prepaid;
+      let quantity = this.postData.quantity;
+      let showdays = this.postData.showdays;
+      //return (
+      //    this.postData.username &&
+      //        this.postData.password &&
+      //        username.length > 0 &&
+      //        password.length > 0
+      //);
+  }
+  /*
+{"title":"1","name":"1","applydays":1,"approvedays":1,"prepaid":1,"quantity":1,"showdays":1,"category":"/api/categories/1","price":1,"description":"1", "sticky": true, "recommended": true, "owner":"/api/users/4", "platform": "/api/platforms/1"}
+   
+   */
 }
