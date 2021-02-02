e5efb5fc0bc04a6f49e45f05bee8b2fd9ebbb2e4
Al Zee <z@alz.ee>
2021-02-02 19:53:37 +0800

    add patch() in httpservice

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -16,6 +16,14 @@ export class HttpService {
         return this.http.post(url, JSON.stringify(data), options);
     }
 
+    patch(serviceName: string, data: any) {
+        const headers = new HttpHeaders('Content-Type: application/merge-patch+json');
+        const options = { headers: headers, withCredintials: false };
+        const url = environment.apiUrl + serviceName;
+
+        return this.http.patch(url, JSON.stringify(data), options);
+    }
+
     get(serviceName: string) {
         const headers = new HttpHeaders('accept: application/json');
         const options = { headers: headers, withCredintials: false };
