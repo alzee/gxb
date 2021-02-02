d23cc9ecd3855981e889f426340cda2d426dd8c6
Al Zee <z@alz.ee>
2021-02-02 13:45:34 +0800

    get applies in mytasks/mytasks

--- src/app/mytasks/mytasks.page.ts
+++ src/app/mytasks/mytasks.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { AuthConstants } from '../config/auth-constants';
+import { StorageService } from '../services/storage.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-mytasks',
@@ -6,60 +10,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mytasks.page.scss'],
 })
 export class MytasksPage implements OnInit {
+  applies = [];
+  applies0 = [];
+  applies1 = [];
+  applies2 = [];
+  applies3 = [];
+  applies4 = [];
+  userData = {};
+  envs = environment;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+      private storageService: StorageService,
+      private httpService: HttpService
+  ) {
   }
 
-  public tasks = [
-    {
-      boss: 'jess',
-      avatar: '../assets/img/she.png',
-      title: '任务名称',
-      runner: 88,
-      left: 12,
-      cate: '简单关注',
-      sticky: true,
-      price: 55.5,
-      tags: ['急速审核', '趣省钱'],
-      date: '2020-11-11',
-      link: '/detail',
-    },
-    {
-      boss: 'mike',
-      avatar: '../assets/img/she.png',
-      title: '任务名称',
-      runner: 88,
-      left: 12,
-      cate: '账号注册',
-      sticky: false,
-      price: 55.5,
-      tags: ['急速审核', '趣省钱'],
-      date: '2020-11-11',
-      link: '/detail',
-    },
-  ];
+  ngOnInit() {
+      this.storageService.get(AuthConstants.AUTH).then((res) => {
+          this.userData = res;
+          this.httpService.get('applies?applicant.id=' + this.userData.id).subscribe((res) => {
+              console.log(res);
+              this.applies0 = res;
+              this.applies = this.applies0;
+          });
+          this.httpService.get('applies?status.id=1&applicant.id=' + this.userData.id).subscribe((res) => {
+              console.log(res);
+              this.applies1 = res;
+          });
+          this.httpService.get('applies?status.id=2&applicant.id=' + this.userData.id).subscribe((res) => {
+              console.log(res);
+              this.applies2 = res;
+          });
+          this.httpService.get('applies?status.id=3&applicant.id=' + this.userData.id).subscribe((res) => {
+              console.log(res);
+              this.applies3 = res;
+          });
+          this.httpService.get('applies?status.id=4&applicant.id=' + this.userData.id).subscribe((res) => {
+              console.log(res);
+              this.applies4 = res;
+          });
+      });
+  }
+
+  segmentChanged(ev: any) {
+    this.applies = this.applies0;
+    switch(ev.detail.value){
+        case this.statuses[0].value:
+            this.applies = this.applies0;
+            break;
+        case this.statuses[1].value:
+            this.applies = this.applies1;
+            break;
+        case this.statuses[2].value:
+            this.applies = this.applies2;
+            break;
+        case this.statuses[3].value:
+            this.applies = this.applies3;
+            break;
+        case this.statuses[4].value:
+            this.applies = this.applies4;
+            break;
+    }
+    console.log('Segment changed', ev.detail.value);
+  }
 
   public statuses = [
     {
-      value: 'all',
+      value: '0',
       label: '全部',
     },
     {
-      value: 'preSubmit',
+      value: '1',
       label: '待提交',
     },
     {
-      value: 'reviewing',
+      value: '2',
       label: '审核中',
     },
     {
-      value: 'fail',
+      value: '3',
       label: '不合格',
     },
     {
-      value: 'done',
+      value: '4',
       label: '已完成',
     },
   ];
