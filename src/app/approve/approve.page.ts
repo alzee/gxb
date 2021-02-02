00def7987b90e667776c29f7601bbc87732f8c38
Al Zee <z@alz.ee>
2021-02-02 19:54:54 +0800

    successfully approve apply

--- src/app/approve/approve.page.ts
+++ src/app/approve/approve.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { HttpService } from '../services/http.service';
+import { ToastService } from '../services/toast.service';
 
 @Component({
   selector: 'app-approve',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./approve.page.scss'],
 })
 export class ApprovePage implements OnInit {
+  applyid = '';
+  data = {
+    status: "/api/statuses/4"
+  };
 
-  constructor() { }
+  constructor(
+    private toastService: ToastService,
+      private activeRoute: ActivatedRoute,
+      private httpService: HttpService,
+      private _location: Location,
+      private router: Router
+  ) { }
 
   ngOnInit() {
+      this.activeRoute.queryParams.subscribe((params: Params) => {
+          this.applyid = params['applyid'];
+      });
   }
+
+  approve() {
+      this.httpService.patch('applies/' + this.applyid, this.data).subscribe((res) => {
+        console.log(res);
+        this.toastService.presentToast('审核通过');
+        this._location.back();
+      });
+  }
+
 
 }
