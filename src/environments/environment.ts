0daf7f6d4f7e782bfc7fd2abf316318b39d1e9d3
Al Zee <z@alz.ee>
2021-02-25 01:49:14 +0800

    show desc and img in detail page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://b.gxb/api/',
-  imgUrl: 'http://b.gxb/uploads/files/img/'
+  imgUrl: 'http://b.gxb/uploads/files/img/',
+  url: 'http://b.gxb'
 };
 
 /*
