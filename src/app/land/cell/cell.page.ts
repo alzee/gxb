73a3b9e8f1d50383aba2f2ad5f91c6ccd9f141b3
Al Zee <z@alz.ee>
2021-03-01 12:44:12 +0800

    page land/cell data

--- src/app/land/cell/cell.page.ts
+++ src/app/land/cell/cell.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HttpService } from '../../services/http.service';
+import { environment } from '../../../environments/environment';
+
+interface Data {
+    [propName: string]: any;
+}
 
 @Component({
   selector: 'app-cell',
@@ -6,10 +13,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cell.page.scss'],
 })
 export class CellPage implements OnInit {
+  url = environment.url;
+  id: number;
+  data: Data;
+  user: string;
 
-  constructor() { }
+  constructor(
+      private activeRoute: ActivatedRoute,
+      private httpService: HttpService
+  ) { }
 
   ngOnInit() {
+      this.activeRoute.queryParams.subscribe((params: Params) => {
+          this.id = params['id'];
+      });
+
+      this.httpService.get('land_posts/' + this.id).subscribe((res) => {
+          this.data = res;
+          console.log(this.data);
+      });
   }
 
 }
