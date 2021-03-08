f2d1ca8963a4ab46acf37b538961ffaa4605ac1f
Al Zee <z@alz.ee>
2021-03-08 18:25:13 +0800

    page approve

--- src/app/approve/approve.page.ts
+++ src/app/approve/approve.page.ts
@@ -3,6 +3,7 @@ import {Location} from '@angular/common';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { HttpService } from '../services/http.service';
 import { ToastService } from '../services/toast.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-approve',
@@ -10,12 +11,15 @@ import { ToastService } from '../services/toast.service';
   styleUrls: ['./approve.page.scss'],
 })
 export class ApprovePage implements OnInit {
+  url = environment.url;
   pass: boolean;
   applyid = '';
   data = {
     //status: "/api/statuses/3",
     status: "/api/statuses/4",
   };
+  guides = [];
+  workPics = [];
 
   constructor(
     private toastService: ToastService,
@@ -28,6 +32,12 @@ export class ApprovePage implements OnInit {
   ngOnInit() {
       this.activeRoute.queryParams.subscribe((params: Params) => {
           this.applyid = params['applyid'];
+        this.httpService.get('applies/' + this.applyid).subscribe((res) => {
+          this.guides = res.task.guides;
+          this.workPics = res.pic;
+          console.log(this.guides);
+          console.log(this.workPics);
+        });
       });
   }
 
