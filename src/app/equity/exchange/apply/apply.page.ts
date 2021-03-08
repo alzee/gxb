92a667b88e128ad5393d340577c37dbfd6607eec
Al Zee <z@alz.ee>
2021-03-08 16:53:46 +0800

    page equity/exchange/apply

--- src/app/equity/exchange/apply/apply.page.ts
+++ src/app/equity/exchange/apply/apply.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthConstants } from '../../../config/auth-constants';
+import { StorageService } from '../../../services/storage.service';
+import { HttpService } from '../../../services/http.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-apply',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apply.page.scss'],
 })
 export class ApplyPage implements OnInit {
+  uid: number;
+  gxb: number;
+  rate: number;
+  equity: number = 0;
 
-  constructor() { }
+  constructor(
+      private httpService: HttpService,
+      private storageService: StorageService
+  ) { }
 
   ngOnInit() {
+    this.storageService.get(AuthConstants.AUTH).then((res) => {
+      this.uid = res.id;
+      this.httpService.get('users/' + this.uid).subscribe((res) => {
+          this.gxb = res.gxb;
+          console.log(this.gxb);
+      });
+    });
+
+    this.httpService.get('configs?page=1&label=exchangePirce').subscribe((res) => {
+        this.rate = 1 / res[0].value;
+        console.log(this.rate);
+    });
   }
 
 }
