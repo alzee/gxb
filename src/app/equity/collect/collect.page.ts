a6d8a7efa2b3f8b5c92a533b20519b2082a6f847
Al Zee <z@alz.ee>
2021-03-08 15:53:15 +0800

    post Gxb in page equity/collect

--- src/app/equity/collect/collect.page.ts
+++ src/app/equity/collect/collect.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { environment } from '../../../environments/environment';
+import { AuthConstants } from '../../config/auth-constants';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'app-collect',
@@ -7,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectPage implements OnInit {
   hists = [];
+  uid: number;
+  postData = {};
+  amount: number = 1;
 
-  constructor() { }
+  constructor(
+      private storageService: StorageService,
+      private httpService: HttpService
+  ) { }
 
   ngOnInit() {
+    this.storageService.get(AuthConstants.AUTH).then((res) => {
+      this.uid = res.id;
+      this.httpService.get('gxbs?page=1&itemsPerPage=30&user=' + this.uid).subscribe((res) => {
+          this.hists = res;
+          console.log(res);
+          console.log(this.uid);
+      });
+    });
+  }
+
+  collectGxb() {
+      this.postData.amount = this.amount;
+      this.postData.user = '/api/users/' + this.uid;
+      this.httpService.post('gxbs', this.postData).subscribe((res) => {
+          console.log(res);
+      });
+      console.log('collected');
+      this.ngOnInit();
   }
 
 }
