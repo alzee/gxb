f32ea921fd0cdbeec6f02ad2d32ecd8cde65c4a2
Al Zee <z@alz.ee>
2021-03-08 14:37:31 +0800

    page land/my

--- src/app/land/my/my.page.ts
+++ src/app/land/my/my.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthConstants } from '../../config/auth-constants';
+import { StorageService } from '../../services/storage.service';
+import { HttpService } from '../../services/http.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-my',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my.page.scss'],
 })
 export class MyPage implements OnInit {
+  envs = environment;
+  income: number = 0;
+  profit: number = 0;
+  transProfit: number = 0;
+  myLands = [];
+  sold = [];
+  uid: number;
+  seg: string = "my";
+  showSold: boolean = false;
 
-  constructor() { }
+  constructor(
+      private storageService: StorageService,
+      private httpService: HttpService
+  ) { }
 
   ngOnInit() {
+    this.storageService.get(AuthConstants.AUTH).then((res) => {
+      this.uid = res.id;
+      console.log(this.uid);
+      this.httpService.get('lands?page=1&itemsPerPage=30&owner.id=' + this.uid).subscribe((res) => {
+          this.myLands = res;
+          console.log(res);
+      });
+    });
   }
 
+  segChanged(ev: any) {
+    console.log('Segment changed', ev.detail.value);
+    console.log(this.seg);
+    if(this.seg == 'my')
+      this.showSold = false;
+    else
+      this.showSold = true;
+  }
 }
