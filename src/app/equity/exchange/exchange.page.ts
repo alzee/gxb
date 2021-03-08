75a464d68c82f783790cc35967d49497c86c4b41
Al Zee <z@alz.ee>
2021-03-08 15:15:52 +0800

    page equity/exchange

--- src/app/equity/exchange/exchange.page.ts
+++ src/app/equity/exchange/exchange.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-exchange',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exchange.page.scss'],
 })
 export class ExchangePage implements OnInit {
+  rate: number;
+  total: number;
+  remain: number = 0;
+  taken: number = 0;
 
-  constructor() { }
+  constructor(
+      private httpService: HttpService
+  ) { }
 
   ngOnInit() {
+      this.httpService.get('configs?page=1&label=exchangePirce').subscribe((res) => {
+          this.rate = 1 / res[0].value;
+          console.log(this.rate);
+      });
+
+      this.httpService.get('configs?page=1&label=quantityGXB').subscribe((res) => {
+          this.total = res[0].value;
+          console.log(this.total);
+      });
   }
 
 }
