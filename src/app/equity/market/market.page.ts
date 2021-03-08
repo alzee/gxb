dbcee6be77f5e47e3a3ea67e94638be78f855ec0
Al Zee <z@alz.ee>
2021-03-08 18:55:13 +0800

    shopId to market/buy

--- src/app/equity/market/market.page.ts
+++ src/app/equity/market/market.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { environment } from '../../../environments/environment';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-market',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./market.page.scss'],
 })
 export class MarketPage implements OnInit {
+  shops = [];
+  shopId: number;
 
-  constructor() { }
+  constructor(
+      private activeRoute: ActivatedRoute,
+      private httpService: HttpService
+  ) { }
 
   ngOnInit() {
+      this.activeRoute.queryParams.subscribe((params: Params) => {
+          this.shopId = params['id'];
+      });
+
+      this.httpService.get('equity_shops?page=1&itemsPerPage=30').subscribe((res) => {
+          this.shops = res;
+          console.log(res);
+      });
   }
 
 }
