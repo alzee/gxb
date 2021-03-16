b612456b5a0b25a721be3c4c42155cbb0c02c81a
Al Zee <z@alz.ee>
2021-03-17 05:39:20 +0800

    page bids

--- src/app/bids/bids.page.ts
+++ src/app/bids/bids.page.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bids.page.scss'],
 })
 export class BidsPage implements OnInit {
+  bids = [
+    {
+      "bid": 99,
+      "price": 300
+    },
+    {
+      "bid": 99,
+      "price": 260
+    },
+    {
+      "bid": 99,
+      "price": 220
+    },
+    {
+      "bid": 99,
+      "price": 180
+    },
+  ];
 
   constructor() { }
 
