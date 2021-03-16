af4c7d5a0d46d72ea4a2b5156c6819d969ceac10
Al Zee <z@alz.ee>
2021-03-17 07:46:46 +0800

    nth

--- src/app/bids/offer/offer.page.ts
+++ src/app/bids/offer/offer.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HttpService } from '../../services/http.service';
+import { environment } from '../../../environments/environment';
+
+interface Data {
+    [propName: string]: any;
+}
 
 @Component({
   selector: 'app-offer',
@@ -6,10 +13,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offer.page.scss'],
 })
 export class OfferPage implements OnInit {
+  position: number;
+  myposts: Data;
+  bids: Data;
+  myBid: number;
+  post: number;
 
-  constructor() { }
+  constructor(
+      private activeRoute: ActivatedRoute,
+      private httpService: HttpService,
+  ) { }
 
   ngOnInit() {
+      this.activeRoute.queryParams.subscribe((params: Params) => {
+          this.position = params['id'];
+      });
+      this.httpService.get('tasks?order%5Bdate%5D=desc').subscribe((res) => {
+        console.log(res);
+        this.myposts = res;
+      });
+      this.httpService.get('bids?page=1&itemsPerPage=10&position=' + this.id).subscribe((res) => {
+        console.log(res);
+        this.bids = res;
+      });
+  }
+
+  bid(){
+    console.log(this.myBid);
+    console.log(this.post);
+    console.log(this.position);
   }
 
 }
