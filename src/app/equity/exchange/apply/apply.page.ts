287d34bdef61f1f8373f59024536b0960cddca97
Al Zee <z@alz.ee>
2021-03-26 07:23:55 +0800

    equity/exchange done

--- src/app/equity/exchange/apply/apply.page.ts
+++ src/app/equity/exchange/apply/apply.page.ts
@@ -3,6 +3,9 @@ import { AuthConstants } from '../../../config/auth-constants';
 import { StorageService } from '../../../services/storage.service';
 import { HttpService } from '../../../services/http.service';
 import { environment } from '../../../../environments/environment';
+import { ToastService } from '../../../services/toast.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 interface Data {
     [propName: string]: any;
@@ -18,12 +21,16 @@ export class ApplyPage implements OnInit {
   gxb: number;
   rate: number;
   equity: number = 0;
+  equityBefore: number;
   max: number;
   userData: Data;
 
   constructor(
       private httpService: HttpService,
-      private storageService: StorageService
+      private storageService: StorageService,
+      private toastService: ToastService,
+      public alertController: AlertController,
+      private router: Router,
   ) { }
 
   ngOnInit() {
@@ -33,6 +40,7 @@ export class ApplyPage implements OnInit {
       this.httpService.get('users/' + this.uid).subscribe((res) => {
           this.userData = res;
           this.gxb = this.userData.gxb;
+          this.equityBefore = this.userData.equity;
           console.log(this.gxb);
       });
     });
@@ -46,5 +54,75 @@ export class ApplyPage implements OnInit {
   buyMax(){
       this.max = this.gxb / this.rate;
       this.equity = Math.trunc(this.max);
+  }
+
+  validate(){
+    if(this.equity == 0)
+      return 1
+  }
+
+  apply0(){
+    // subtract gxb
+    let gxbAfter = this.gxb - this.equity * this.rate;
+    // add equity
+    let equityAfter = this.equityBefore + this.equity;
+    // subtract platform equity
+    console.log(gxbAfter, equityAfter);
+    if(this.validate() == 1){
+      this.toastService.presentToast('请输入购买的股权数量');
+    }
+    else{
+      let data = {
+        "gxb": gxbAfter,
+        "equity": equityAfter
+      };
+      this.httpService.patch('users/' + this.uid, data).subscribe((res) => {
+        console.log(res);
+      });
+    }
+  }
+
+  async apply() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'GXB 兑换股权！',
+      message: '此交易无法撤销！确定要兑换么？',
+      buttons: [
+        {
+          text: '取消',
+          role: 'cancel',
+          cssClass: 'danger',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: '确认',
+          cssClass: 'danger',
+          handler: () => {
+            // subtract gxb
+            let gxbAfter = this.gxb - this.equity * this.rate;
+            // add equity
+            let equityAfter = this.equityBefore + this.equity;
+            // subtract platform equity
+            console.log(gxbAfter, equityAfter);
+            if(this.validate() == 1){
+              this.toastService.presentToast('请输入购买的股权数量');
+            }
+            else{
+              let data = {
+                "gxb": gxbAfter,
+                "equity": equityAfter
+              };
+              this.httpService.patch('users/' + this.uid, data).subscribe((res) => {
+                console.log(res);
+                this.toastService.presentToast('兑换成功！');
+                this.router.navigate(['/equity/exchange']);
+              });
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 }
