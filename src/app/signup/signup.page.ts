1996066abd942bc4130c11a17eb7bc40cf3fb915
Al Zee <z@alz.ee>
2021-03-29 17:05:14 +0800

    page signup validate

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -4,6 +4,8 @@ import { AuthConstants } from '../config/auth-constants';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ToastService } from '../services/toast.service';
+import { HttpService } from '../services/http.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-signup',
@@ -11,6 +13,16 @@ import { ToastService } from '../services/toast.service';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+    smsType: string = 'register';
+    smsPass: string;
+    smsResp;
+    username: string;
+    password: string;
+    phone: string;
+    vCode: string;
+    term = {
+        isChecked: true
+    }
     postData = {
         username: '',
         password: '',
@@ -21,54 +33,71 @@ export class SignupPage implements OnInit {
         private authService: AuthService,
         private toastService: ToastService,
         private storageService: StorageService,
+        private httpService: HttpService,
         private router: Router
-    ) {}
+    ) {
+        this.smsPass = environment.smsPass;
+    }
 
     ngOnInit() {}
 
     validateInputs() {
-        let username = this.postData.username.trim();
-        let password = this.postData.password.trim();
-        let phone = this.postData.phone.trim();
-        return (
-            this.postData.username &&
-                this.postData.password &&
-                this.postData.phone &&
-                username.length > 0 &&
-                phone.length > 0 &&
-                password.length > 0
-        );
+        if(!this.smsResp){
+            return 1
+        }
+        else if(this.vCode != this.smsResp.code){
+            return 2
+        }
     }
 
-    signupAction() {
-        if (this.validateInputs()) {
-            this.authService.signup(this.postData).subscribe(
-                (res: any) => {
-                    console.log(res);
-                    if (res) {
-                        // Storing the User data.
-                        this.storageService
-                        .store(AuthConstants.AUTH, res)
-                        .then(res => {
-                            this.router.navigate(['/tabs/home']);
-                        });
-                    } else {
-                        this.toastService.presentToast(
-                            '用户名已存在'
-                        );
+    signup() {
+        console.log(this.smsResp);
+        switch(this.validateInputs()){
+            case 1:
+                this.toastService.presentToast('请获取验证码');
+                break;
+            case 2:
+                this.toastService.presentToast('验证码错误');
+                break;
+            default:
+                this.postData.username = this.username;
+                this.postData.password = this.password;
+                this.postData.phone = this.phone;
+                this.authService.signup(this.postData).subscribe(
+                    (res: any) => {
+                        console.log(res);
+                        if (res) {
+                            // Storing the User data.
+                            this.storageService
+                            .store(AuthConstants.AUTH, res)
+                            .then(res => {
+                                this.router.navigate(['/tabs/home']);
+                            });
+                        } else {
+                            this.toastService.presentToast(
+                                '用户名已存在'
+                            );
+                        }
+                    },
+                    (error: any) => {
+                        this.toastService.presentToast('网络异常');
                     }
-                },
-                (error: any) => {
-                    this.toastService.presentToast('网络异常');
-                }
-            );
-        } else {
-            this.toastService.presentToast(
-                '请输入用户名和密码'
             );
         }
     }
 
-    presentToast(){
+    getSms(){
+      console.log(this.regForm);
+      console.log(this.phone);
+      console.log(this.smsType);
+      console.log(this.smsPass);
+      //if(){
+      //    this.toastService.presentToast('请获取验证码');
+      //}
+      this.httpService.get(`sms?phone=${this.phone}&type=${this.smsType}&pass=${this.smsPass}`).subscribe((res) => {
+          this.toastService.presentToast('验证码已发送');
+          this.smsResp= res;
+          console.log(res);
+      });
     }
 }
