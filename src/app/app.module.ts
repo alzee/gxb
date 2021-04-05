49762a699d3dd654dd3f13202f58db2229db90d2
Al Zee <z@alz.ee>
2021-04-06 02:26:08 +0800

    import Wechat in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Wechat } from '@ionic-native/wechat/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
+    Wechat,
     StatusBar,
     SplashScreen,
     HttpClientModule,
--- src/app/topup/topup.page.ts
+++ src/app/topup/topup.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params, RoutesRecognized } from '@angular/router';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { ToastService } from '../services/toast.service';
+import { Platform } from '@ionic/angular';
+import { Wechat } from '@ionic-native/wechat/ngx';
+// declare var Wechat:any;
 
 @Component({
   selector: 'app-topup',
@@ -12,6 +16,9 @@ export class TopupPage implements OnInit {
   form: FormGroup;
 
   constructor(
+      private wechat: Wechat,
+      private platform: Platform,
+      private toastService: ToastService,
       private formBuilder: FormBuilder,
       private activeRoute: ActivatedRoute
   ) { }
@@ -39,7 +46,43 @@ export class TopupPage implements OnInit {
   }
 
   topup(){
-    console.log(this.amount);
-  }
+    // console.log(this.amount);
+    this.platform.ready().then(() => {
+        // this.wechat.isInstalled(function (installed) {
+        //     this.toastService.presentToast("Wechat installed: " + (installed ? "Yes" : "No"));
+        // }, function (reason) {
+        //     this.toastService.presentToast("Failed: " + reason);
+        // });
 
+        // alert("Wechat installed: " + (this.wechat.isInstalled() ? "Yes" : "No"));
+        //this.wechat.isInstalled(function (installed) {
+        //    alert("Wechat installed: " + (installed ? "Yes" : "No"));
+        //}, function (reason) {
+        //    alert("Failed: " + reason);
+        //});
+
+        let params = {
+            partnerid: '10000100', // merchant id
+            prepayid: 'wx201411101639507cbf6ffd8b0779950874', // prepay id
+            noncestr: '1add1a30ac87aa2db72f57a2375d8fec', // nonce
+            timestamp: '1439531364', // timestamp
+            sign: '0CB01533B8C1EF103065174F50BCA001', // signed string
+        };
+
+        /*
+        this.wechat.isInstalled().then((res) => {
+            alert('fuck yeah');
+        }, reason => {
+            alert('fuck no:' + reason);
+        });
+       */
+
+        this.wechat.sendPaymentRequest(params)
+        .then((resp) => {
+            console.log('fuck');
+        }, reason => {
+            console.log('fuck no:' + reason );
+        });
+    });
+  }
 }
