67acc193cecd4b8b37cd5aace9c79a810e0a608c
Al Zee <z@alz.ee>
2021-04-06 13:27:55 +0800

    almost, maybe

--- src/app/topup/topup.page.ts
+++ src/app/topup/topup.page.ts
@@ -2,9 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params, RoutesRecognized } from '@angular/router';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { ToastService } from '../services/toast.service';
+import { HttpService } from '../services/http.service';
 import { Platform } from '@ionic/angular';
 import { Wechat } from '@ionic-native/wechat/ngx';
-// declare var Wechat:any;
+
+interface Data {
+    [propName: string]: any;
+}
 
 @Component({
   selector: 'app-topup',
@@ -14,13 +18,21 @@ import { Wechat } from '@ionic-native/wechat/ngx';
 export class TopupPage implements OnInit {
   min: number;
   form: FormGroup;
+  data: Data;
+  partnerid: string;
+  prepayid: string;
+  noncestr: string;
+  timestamp: string;
+  sig: string;
+
 
   constructor(
       private wechat: Wechat,
       private platform: Platform,
       private toastService: ToastService,
       private formBuilder: FormBuilder,
-      private activeRoute: ActivatedRoute
+      private activeRoute: ActivatedRoute,
+      private httpService: HttpService
   ) { }
 
   ngOnInit() {
@@ -46,43 +58,42 @@ export class TopupPage implements OnInit {
   }
 
   topup(){
-    // console.log(this.amount);
-    this.platform.ready().then(() => {
-        // this.wechat.isInstalled(function (installed) {
-        //     this.toastService.presentToast("Wechat installed: " + (installed ? "Yes" : "No"));
-        // }, function (reason) {
-        //     this.toastService.presentToast("Failed: " + reason);
-        // });
+      this.httpService.get('prepayid').subscribe((res) => {
+          console.log(res);
+          this.data = res;
+          console.log(this.data);
+          this.partnerid = this.data.mchid;
+          this.prepayid = this.data.prepay_id;
+          this.noncestr = this.data.nonce;
+          this.timestamp = this.data.timestamp;
+          this.sig = this.data.sig;
 
-        // alert("Wechat installed: " + (this.wechat.isInstalled() ? "Yes" : "No"));
-        //this.wechat.isInstalled(function (installed) {
-        //    alert("Wechat installed: " + (installed ? "Yes" : "No"));
-        //}, function (reason) {
-        //    alert("Failed: " + reason);
-        //});
-
-        let params = {
-            partnerid: '10000100', // merchant id
-            prepayid: 'wx201411101639507cbf6ffd8b0779950874', // prepay id
-            noncestr: '1add1a30ac87aa2db72f57a2375d8fec', // nonce
-            timestamp: '1439531364', // timestamp
-            sign: '0CB01533B8C1EF103065174F50BCA001', // signed string
-        };
-
-        /*
-        this.wechat.isInstalled().then((res) => {
-            alert('fuck yeah');
-        }, reason => {
-            alert('fuck no:' + reason);
-        });
-       */
-
-        this.wechat.sendPaymentRequest(params)
-        .then((resp) => {
-            console.log('fuck');
-        }, reason => {
-            console.log('fuck no:' + reason );
-        });
-    });
+          let params = {
+              partnerid: this.partnerid,
+              // partnerid: '1606036532',
+              prepayid: this.prepayid,
+              noncestr: this.noncestr,
+              timestamp: this.timestamp,
+              sign: this.sig
+          };
+          console.log(params);
+          this.platform.ready().then(() => {
+              // this.wechat.isInstalled(function (installed) {
+              //     this.toastService.presentToast("Wechat installed: " + (installed ? "Yes" : "No"));
+              // }, function (reason) {
+              //     this.toastService.presentToast("Failed: " + reason);
+              // });
+              
+              // this.wechat.sendPaymentRequest(params).then((res) => {
+              //     alert(params);
+              // }, reason => {
+              // });
+              this.wechat.sendPaymentRequest(params, function () {
+                  alert("Success");
+              }, function (reason) {
+                  alert("Failed: " + reason);
+              });
+          });
+      });
   }
 }
