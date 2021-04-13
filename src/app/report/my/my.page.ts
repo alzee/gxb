14fe78431a1a802d942ff69e3ce26fb3a4bef45e
Al Zee <z@alz.ee>
2021-04-13 10:43:03 +0800

    nth

--- src/app/report/my/my.page.ts
+++ src/app/report/my/my.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { AuthConstants } from '../../config/auth-constants';
+import { StorageService } from '../../services/storage.service';
+
+interface Data {
+    [propName: string]: any;
+}
 
 @Component({
   selector: 'app-my',
@@ -6,10 +13,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my.page.scss'],
 })
 export class MyPage implements OnInit {
+  myReports = [];
+  userData: Data;
 
-  constructor() { }
+  constructor(
+    private storageService: StorageService,
+    private httpService: HttpService
+  ) { }
 
   ngOnInit() {
+    this.storageService.get(AuthConstants.AUTH).then((res) => {
+      this.userData = res;
+      this.httpService.get('reports?apply.applicant=' + this.userData.id).subscribe((res1) => {
+        console.log(res1);
+        this.myReports = res1;
+      });
+    });
   }
 
 }
