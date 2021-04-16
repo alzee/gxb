f9fb07b665609ba49e6818aaf2409f93075247ce
Al Zee <z@alz.ee>
2021-04-16 22:25:07 +0800

    nth

--- src/app/update.sh
+++ src/app/update.sh
@@ -9,7 +9,13 @@
 ############### Main Part ###############
 
 eval $(grep com.drgxb.app ../../config.xml | cut -d' ' -f3)
-[ "$version" ] && return
+
+if [ "$version" ]; then
+    echo $version
+else
+    echo no version found
+    exit
+fi
 
 # scp ../../platforms/android/app/build/outputs/apk/debug/app-debug.apk $ali:w/b.gxb/public/download
 node ../../node_modules/cordova-plugin-apkupdater/src/nodejs/create-manifest.js $version 100k ../../platforms/android/app/build/outputs/apk/debug/app-debug.apk update
