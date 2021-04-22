3b4b9322be15e54a9038641b4f57b961f3c0eef8
Al Zee <z@alz.ee>
2021-04-22 11:26:54 +0800

    兑股交易页面现在显示拥有的股权数量

--- src/app/equity/market/my/my.page.ts
+++ src/app/equity/market/my/my.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../../services/http.service';
 import { environment } from '../../../../environments/environment';
 import { ActivatedRoute, Params } from '@angular/router';
+import { AuthConstants } from '../../../config/auth-constants';
+import { StorageService } from '../../../services/storage.service';
 
 interface Data {
     [propName: string]: any;
@@ -18,10 +20,13 @@ export class MyPage implements OnInit {
   min: number;
   count: number;
   price: number;
-  shopId: number;
-  shop: Data;
+  configs: Array<Data>;
+  userData: Data;
+  user: Data;
+  myEquity: number;
 
   constructor(
+      private storageService: StorageService,
       private activeRoute: ActivatedRoute,
       private httpService: HttpService
   ) {
@@ -31,16 +36,25 @@ export class MyPage implements OnInit {
   }
 
   ngOnInit() {
-      this.activeRoute.queryParams.subscribe((params: Params) => {
-          this.shopId = params.id;
-          this.httpService.get('equity_shops?page=1&id=' + this.shopId).subscribe((res) => {
-              this.shop = res[0];
-              this.price = this.shop.price;
-              console.log(this.shop);
+      this.storageService.get(AuthConstants.AUTH).then(
+          (res) => {
+              this.userData = res;
+              this.httpService.get('users/' + this.userData.id).subscribe((res) => {
+                  this.user = res;
+                  this.myEquity = this.user.equity;
+              });
           });
+      this.httpService.get('configs?itemsPerPage=30').subscribe((res) => {
+          this.configs = res;
+          for (const i of this.configs) {
+              if (i.label === 'equityPrice') {
+                  this.price = i.value;
+              }
+          }
+          console.log(res);
       });
 
-      this.httpService.get('equity_trades?page=1&itemsPerPage=30').subscribe((res) => {
+      this.httpService.get('equity_trades?page=1&itemsPerPage=10').subscribe((res) => {
           this.hists = res;
           console.log(res);
       });
