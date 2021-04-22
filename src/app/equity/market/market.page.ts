0e55239bcbd8c92658b8d5d6e90d82243737f85d
Al Zee <z@alz.ee>
2021-04-23 04:17:38 +0800

    nth

--- src/app/equity/market/market.page.ts
+++ src/app/equity/market/market.page.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../services/http.service';
 import { environment } from '../../../environments/environment';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
+
+interface Data {
+    [propName: string]: any;
+}
 
 @Component({
   selector: 'app-market',
@@ -9,17 +14,37 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./market.page.scss'],
 })
 export class MarketPage implements OnInit {
-  shops = [];
+  message: Data;
+  orderType = 5;
+  orderNote = '购买股权';
 
   constructor(
-      private httpService: HttpService
+      private httpService: HttpService,
+      private router: Router,
+      private data: DataService
   ) { }
 
   ngOnInit() {
-      this.httpService.get('equity_shops?page=1&itemsPerPage=30').subscribe((res) => {
+      this.httpService.get('equity_trades?page=1&itemsPerPage=30').subscribe((res) => {
           this.shops = res;
           console.log(res);
       });
   }
 
+  buy(i: Data){
+    const orderData = {
+      type: this.orderType,
+      note: this.orderNote,
+      amount: i.rmb,
+      data: {
+        entityId: i.id,
+        // postData
+      }
+    };
+    this.message = {
+      orderData
+    };
+    this.data.changeMessage(this.message);
+    this.router.navigate(['/pay'], { replaceUrl: true });
+  }
 }
