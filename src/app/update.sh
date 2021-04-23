e688c3c0b51378bc73efa50304a44136d64f8d95
Al Zee <z@alz.ee>
2021-04-23 19:07:05 +0800

    nth

--- src/app/update.sh
+++ src/app/update.sh
@@ -19,4 +19,4 @@ fi
 
 # scp ../../platforms/android/app/build/outputs/apk/debug/app-debug.apk $ali:w/b.gxb/public/download
 node ../../node_modules/cordova-plugin-apkupdater/src/nodejs/create-manifest.js $version 100k ../../platforms/android/app/build/outputs/apk/debug/app-debug.apk update
-rsync -avP update/ $ali:w/b.gxb/public/update/
+rsync -avP update/ gxb.alz.ee:/www/wwwroot/backend.drgxb.com/public/update/
