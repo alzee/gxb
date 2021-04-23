1537f6100998ed64a4915b9c20f1132509d28030
Al Zee <z@alz.ee>
2021-04-23 13:37:54 +0800

    page terms

--- src/app/terms/terms.page.ts
+++ src/app/terms/terms.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+interface Data {
+    [propName: string]: any;
+}
 
 @Component({
   selector: 'app-terms',
@@ -6,10 +12,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./terms.page.scss'],
 })
 export class TermsPage implements OnInit {
+  id: number;
+  node: Data;
 
-  constructor() { }
+  constructor(
+      private activeRoute: ActivatedRoute,
+      private httpService: HttpService
+  ) { }
 
   ngOnInit() {
-  }
+    this.activeRoute.queryParams.subscribe((params: Params) => {
+      this.id = parseInt(params.id, 10);
+      this.httpService.get('nodes/' + this.id).subscribe((res) => {
+        console.log(res);
+        this.node = res;
+      });
+    });
+}
 
 }
