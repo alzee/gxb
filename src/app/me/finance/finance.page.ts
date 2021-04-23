70043108b747b0ca050c46f126016640a549f6a4
Al Zee <z@alz.ee>
2021-04-23 12:35:44 +0800

    账户明细可以正常显示数据了

--- src/app/me/finance/finance.page.ts
+++ src/app/me/finance/finance.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { AuthConstants } from '../../config/auth-constants';
+import { StorageService } from '../../services/storage.service';
+
+interface Data {
+    [propName: string]: any;
+}
 
 @Component({
   selector: 'app-finance',
@@ -6,20 +13,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./finance.page.scss'],
 })
 export class FinancePage implements OnInit {
-  subtotal: number;
+  seg = 0;
+  userData: Data;
   month: string;
   hists = [];
 
-  constructor() {
-    this.subtotal = 0;
+  constructor(
+    private httpService: HttpService,
+    private storageService: StorageService
+  ) {
     const d = new Date();
     this.month = d.getFullYear() + '-' + (d.getMonth() + 1);
   }
 
   ngOnInit() {
+    this.storageService.get(AuthConstants.AUTH).then(
+      (res) => {
+        this.userData = res;
+        this.httpService.get('finances?page=1&itemsPerPage=30&order%5Bdate%5D=desc&user=' + this.userData.id).subscribe((res) => {
+          this.hists = res;
+          console.log(res);
+        });
+      });
   }
 
   showMonth() {
     console.log(this.month);
+  }
+
+  segmentChanged(ev: any) {
+    this.seg = ev.detail.value;
+    console.log('Segment changed', this.seg);
   }
 }
