b7aaa88dbc81b06aea59478ed4f41c8bfb4e1d93
Al Zee <z@alz.ee>
2021-04-24 18:22:35 +0800

    update.sh

--- src/app/update.sh
+++ src/app/update.sh
@@ -3,6 +3,7 @@
 # vim:ft=sh
 
 ############### Variables ###############
+path=gxb.alz.ee:/www/wwwroot/backend.drgxb.com/public
 
 ############### Functions ###############
 
@@ -17,6 +18,7 @@ else
     exit
 fi
 
-# scp ../../platforms/android/app/build/outputs/apk/debug/app-debug.apk $ali:w/b.gxb/public/download
+# scp ../../platforms/android/app/build/outputs/apk/debug/app-debug.apk $path/download
 node ../../node_modules/cordova-plugin-apkupdater/src/nodejs/create-manifest.js $version 100k ../../platforms/android/app/build/outputs/apk/debug/app-debug.apk update
-rsync -avP update/ gxb.alz.ee:/www/wwwroot/backend.drgxb.com/public/update/
+rsync -avP update/ $path/update/
+rm -rf update/
