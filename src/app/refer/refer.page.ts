fba1c48474a1a5355fa27afcf06d9cdbe9d84ac4
Al Zee <z@alz.ee>
2021-04-27 17:20:08 +0800

    邀请好友页面现在可以显示好友人数了

--- src/app/refer/refer.page.ts
+++ src/app/refer/refer.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { AuthConstants } from '../config/auth-constants';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-refer',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./refer.page.scss'],
 })
 export class ReferPage implements OnInit {
+  friends = [];
+  count: number;
 
-  constructor() { }
+  constructor(
+      private httpService: HttpService,
+      private storageService: StorageService
+  ) { }
 
   ngOnInit() {
+      this.storageService.get(AuthConstants.AUTH).then((res) => {
+          this.userData = res;
+          this.httpService.get('users?page=1&itemsPerPage=1000&referrer=' + this.userData.id).subscribe((res1) => {
+              this.friends = res1;
+              this.count = this.friends.length;
+          });
+      });
   }
 
 }
