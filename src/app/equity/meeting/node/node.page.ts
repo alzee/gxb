274ec9b0d8051a0e5f6551933ced8e2a3055860c
Al Zee <z@alz.ee>
2021-05-10 20:18:57 +0800

    can vote now

--- src/app/equity/meeting/node/node.page.ts
+++ src/app/equity/meeting/node/node.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../../services/http.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { AuthConstants } from '../../../config/auth-constants';
+import { StorageService } from '../../../services/storage.service';
 
 interface Data {
     [propName: string]: any;
@@ -13,22 +15,47 @@ interface Data {
 })
 export class NodePage implements OnInit {
   node: Data;
-  id: number;
+  nid: number;
+  userData: Data;
+  voted = true;
 
   constructor(
       private activeRoute: ActivatedRoute,
+      private storageService: StorageService,
       private httpService: HttpService
   ) { }
 
   ngOnInit() {
       this.activeRoute.queryParams.subscribe((params: Params) => {
-          this.id = params.id;
+          this.nid = params.id;
       });
 
-      this.httpService.get('nodes/' + this.id).subscribe((res) => {
+      this.storageService.get(AuthConstants.AUTH).then(
+          (res) => {
+              this.userData = res;
+              this.httpService.get(`votes?user=${this.userData.id}&node=${this.nid}`).subscribe((res1) => {
+                  if (res1.length === 0) {
+                      this.voted = false;
+                  }
+              });
+          });
+
+      this.httpService.get('nodes/' + this.nid).subscribe((res) => {
           this.node = res;
           console.log(this.node);
       });
   }
 
+  vote(isUp: boolean) {
+      const postData = {
+          user: '/api/users/' + this.userData.id,
+          node: '/api/nodes/' + this.nid,
+          isUp
+      };
+
+      this.httpService.post('votes', postData).subscribe((res) => {
+          console.log(res);
+          this.voted = true;
+      });
+  }
 }
