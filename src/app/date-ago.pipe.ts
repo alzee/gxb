d323c8db67a81a2b0773a37f6dae5071bb1702f7
Al Zee <z@alz.ee>
2021-05-11 05:00:47 +0800

    translate dateAgo pipe

--- src/app/date-ago.pipe.ts
+++ src/app/date-ago.pipe.ts
@@ -10,25 +10,22 @@ export class DateAgoPipe implements PipeTransform {
         if (value) {
             const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
             if (seconds < 29) // less than 30 seconds ago will show as 'Just now'
-                return 'Just now';
+                return '刚刚';
             const intervals = {
-                'year': 31536000,
-                'month': 2592000,
-                'week': 604800,
-                'day': 86400,
-                'hour': 3600,
-                'minute': 60,
-                'second': 1
+                '年': 31536000,
+                '个月': 2592000,
+                '周': 604800,
+                '天': 86400,
+                '小时': 3600,
+                '分钟': 60,
+                '秒': 1
             };
             let counter;
             for (const i in intervals) {
                 counter = Math.floor(seconds / intervals[i]);
-                if (counter > 0)
-                    if (counter === 1) {
-                        return counter + ' ' + i + ' ago'; // singular (1 day ago)
-                    } else {
-                        return counter + ' ' + i + 's ago'; // plural (2 days ago)
-                    }
+                if (counter > 0) {
+                    return counter + ' ' + i + '前'; // singular (1 day ago)
+                }
             }
         }
         return value;
