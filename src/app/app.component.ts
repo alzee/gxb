8f5346f0028407f831c260dff18f9ef148c070d7
Al Zee <z@alz.ee>
2021-05-17 01:00:27 +0800

    nth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { HttpService } from './services/http.service';
+import { AppMinimize } from '@ionic-native/app-minimize/ngx';
+import { AppVersion } from '@ionic-native/app-version/ngx';
+import { environment } from '../environments/environment';
+
+interface Data {
+    [propName: string]: any;
+}
+
+declare var cordova;
 
 @Component({
   selector: 'app-root',
@@ -10,9 +20,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  env = environment;
+  conf: Data;
+  ver: string;
+  updateUrl = this.env.updateUrl;
+  check: Data;
   constructor(
     private platform: Platform,
+    private appVersion: AppVersion,
+    public alertController: AlertController,
     private splashScreen: SplashScreen,
+    private httpService: HttpService,
+    private appMinimize: AppMinimize,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
@@ -22,6 +41,86 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.httpService.get('confs/1').subscribe((res) => {
+          console.log(res);
+          this.conf = res;
+          if (this.conf.forceUpdate) {
+              this.appVersion.getVersionNumber().then(
+                  (res) => {
+                      this.ver = res;
+                  }
+              );
+
+              cordova.plugins.apkupdater.check(this.updateUrl).then(
+                  (res) => {
+                      this.check = res;
+                      if (res.version !== this.ver) {
+                          this.alert();
+                      }
+                  }
+              );
+          }
+      });
     });
+  }
+
+  async alert() {
+      const alert = await this.alertController.create({
+          header: '更新至最新版',
+          message: '请更新至最新版',
+          backdropDismiss: false,
+          buttons: [
+              {
+                  text: '取消',
+                  role: 'cancel',
+                  cssClass: 'secondary',
+                  handler: (blah) => {
+                      console.log('Confirm Cancel: blah');
+                      this.appMinimize.minimize();
+                  }
+              }, {
+                  text: '确定',
+                  handler: () => {
+                      console.log('Confirm Okay');
+                      this.update();
+                  }
+              }
+          ]
+      });
+
+      await alert.present();
+  }
+
+  update() {
+      cordova.plugins.apkupdater.check(this.updateUrl).then(
+          (res) => {
+              if (this.check.ready) {
+                  cordova.plugins.apkupdater.install().then(
+                      (res2) => {
+                          console.log('install done:', res2);
+                      }, (reason2) => {
+                          console.log('install failed:', reason2);
+                      }
+                  );
+              }
+              else {
+                  // show process
+                  cordova.plugins.apkupdater.download().then(
+                      (res1) => {
+                          console.log('download done:', res1);
+                          cordova.plugins.apkupdater.install().then(
+                              (res2) => {
+                                  console.log('install done:', res2);
+                              }, (reason2) => {
+                                  console.log('install failed:', reason2);
+                              }
+                          );
+                      }, (reason1) => {
+                          console.log('download failed:', reason1);
+                      }
+                  );
+              }
+          }
+      );
   }
 }
