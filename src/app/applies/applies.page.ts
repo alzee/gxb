0955d1be4e74d02ba2aca47946eb8d062154772a
Al Zee <z@alz.ee>
2021-05-21 19:49:23 +0800

    applies infinite load

--- src/app/applies/applies.page.ts
+++ src/app/applies/applies.page.ts
@@ -15,6 +15,7 @@ export class AppliesPage implements OnInit {
   taskId = '';
   applies = [];
   envs = environment;
+  page = 1;
   public statuses = [
     {
       value: '0',
@@ -49,10 +50,7 @@ export class AppliesPage implements OnInit {
   ngOnInit() {
       this.activeRoute.queryParams.subscribe((params: Params) => {
           this.taskId = params.task;
-          this.httpService.get('applies?order%5Bid%5D=desc&task.id=' + this.taskId).subscribe((res) => {
-              console.log(res);
-              this.applies = res;
-          });
+          this.getApplies();
       });
   }
 
@@ -60,5 +58,21 @@ export class AppliesPage implements OnInit {
       if (i.status.id >= 12) {
           this.router.navigate(['/approve'], {queryParams: {applyid: i.id}});
       }
+  }
+
+  getApplies() {
+      this.httpService.get(`applies?itemsPerPage=7&page=${this.page}&order%5Bid%5D=desc&task.id=${this.taskId}`).subscribe((res) => {
+          console.log(res);
+          this.applies = [...this.applies, ...res];
+      });
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+      this.page += 1;
+      this.getApplies();
+    }, 500);
   }
 }
