2c4c0f33f2ac69526b6190b7d03d7877d13d1574
Al Zee <z@alz.ee>
2021-05-25 01:43:22 +0800

    chpasswd done

--- src/app/chpasswd/chpasswd.module.ts
+++ src/app/chpasswd/chpasswd.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,6 +13,7 @@ import { ChpasswdPage } from './chpasswd.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     ChpasswdPageRoutingModule
   ],
   declarations: [ChpasswdPage]
--- src/app/chpasswd/chpasswd.page.ts
+++ src/app/chpasswd/chpasswd.page.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { AuthConstants } from '../config/auth-constants';
+import { StorageService } from '../services/storage.service';
+import { ToastService } from '../services/toast.service';
+import { ValidatorFn, AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+
+interface Data {
+    [propName: string]: any;
+}
 
 @Component({
   selector: 'app-chpasswd',
@@ -6,10 +16,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chpasswd.page.scss'],
 })
 export class ChpasswdPage implements OnInit {
+  userData: Data;
+  resp: Data;
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+      public navCtrl: NavController,
+      private formBuilder: FormBuilder,
+      private httpService: HttpService,
+      private toastService: ToastService,
+      private storageService: StorageService
+  ) { }
 
   ngOnInit() {
+      this.storageService.get(AuthConstants.AUTH).then(
+          (res) => {
+              this.userData = res;
+          }
+      );
+
+      this.form = this.formBuilder.group({
+          oldPass: [''],
+          newPass: [''],
+          newPass1: ['']
+      });
   }
 
+  get oldPass(){
+      return this.form.get('oldPass');
+  }
+  
+  get newPass(){
+      return this.form.get('newPass');
+  }
+
+  get newPass1(){
+      return this.form.get('newPass1');
+  }
+
+  chpass() {
+      const postData = {
+          uid: this.userData.id,
+          pass: this.oldPass.value,
+          type: 0
+      };
+      this.httpService.post('chkpass', postData).subscribe((res) => {
+          this.resp = res;
+          if (this.resp.code === 0) {
+              const postData1 = {
+                  password: this.newPass.value
+              };
+              this.httpService.patch('users/' + this.userData.id, postData1).subscribe((res1) => {
+                  console.log(res1);
+                  this.toastService.presentToast('密码已修改');
+                  this.navCtrl.back();
+              }
+              );
+          }
+          else {
+              this.toastService.presentToast('原密码输入错误');
+          }
+      }
+      );
+  }
 }
